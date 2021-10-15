147a479612aa92b8aba6948351347a37cdf1c508
Benoit Beaumont <benoit.beaumont97@gmail.com>
2021-10-15 14:45:19 +0200

    Compte class creation

--- src/fr/banque/Compte.java
+++ src/fr/banque/Compte.java
@@ -1,5 +1,42 @@
 package fr.banque;
 
 public class Compte {
+	private int numero; 
+	private double solde;
+	
+	
+	Compte () {		
+	}
 
+	public Compte(int numero, double solde) {
+		this.numero = numero;
+		this.solde = solde;
+	}
+
+	
+	private void ajouter(double unMontant) {
+		this.solde += unMontant;
+	}
+	
+	
+	private void retirer(double unMontant) {
+		this.solde -= unMontant;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Compte [numero=" + numero + ", solde=" + solde + "]";
+	}
+
+	
+	
+	
+	
 }
